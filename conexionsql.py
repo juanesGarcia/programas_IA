3e74e078bc53c2ecf40befd744270e0a0892b835
juanes <juanesgym2018@gmail.com>
2022-06-20 13:53:19 -0500

    acomodando la parte de mostrar los datos antiguos y actuales

--- conexionsql.py
+++ conexionsql.py
@@ -1,16 +1,19 @@
 import psycopg2 as cv
 import numpy as nm
+from os import system
 conexion1 = cv.connect(database="tiendaElectronica", user="postgres", password="Gm+2001juangym")
 cursor1=conexion1.cursor()
+cursor2=conexion1.cursor()
 cursor1.execute("SELECT * from ejemplo01.musicos")
 for fila in cursor1:
  print(fila)
-cursor1.execute("insert into ejemplo01.musicos (codmusico,nombre,apellido,ciudad_origen) values('57','ryan','castro','medallo')")
-#cursor1.execute("delete from ejemplo01.musicos where nombre='maluma'")
+cursor2.execute("insert into ejemplo01.musicos (codmusico,nombre,apellido,ciudad_origen) values('57','ñengo','flow','pr')")
+#cursor2.execute("delete from ejemplo01.musicos where nombre='ñengo'")
 conexion1.commit()
-cursor1.execute("SELECT * from ejemplo01.musicos")
+cursor2.execute("SELECT * from ejemplo01.musicos")
 print(' \n\nse insertaron los datos nuevos: \n\n')
-usuarios=cursor1.fetchall()
-for u in usuarios:
-  print(u)
+
+for fila in cursor2:
+  print(fila)
+
 conexion1.close()
